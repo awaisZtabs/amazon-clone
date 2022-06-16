844b7bbba1031302aebdf7e7880e1436def00631
awaisX64 <awaisali11150@gmail.com>
2022-06-16 12:08:14 +0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBc6cZseEDKaqSkfjjaKDBjHwbEWVX_7G0",
+  authDomain: "clone-dd9c1.firebaseapp.com",
+  projectId: "clone-dd9c1",
+  storageBucket: "clone-dd9c1.appspot.com",
+  messagingSenderId: "268905338009",
+  appId: "1:268905338009:web:6d9ff5730db68e65809408",
+  measurementId: "G-TN3D3N6SP2"
+};
