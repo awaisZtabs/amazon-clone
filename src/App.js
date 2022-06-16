ccc41fd1d8fd83b5f6759cc695645ba3c8dfd3e1
awaisX64 <awaisali11150@gmail.com>
2022-06-16 16:02:44 +0500

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-     <Header> </Header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+              <h1>IM AM CGECOUT</h1>
+          </Route>
+          <Route   path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>  
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function Header() {
   return (
     <div className="header">
@@ -10,6 +12,7 @@ function Header() {
       />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
@@ -23,6 +26,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">1</span>
         </div>
       </div>
     </div>
